f88cac5bae1bfe77cf66c282c6aa7f0d60f42e91
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-19 15:27:10 -0300

    did the testing

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,9 +1,10 @@
-const {Triangle, Circle, Square, createSVG }= require('../lib/Shape.js')
+const { Triangle, Circle, Square, createSVG } = require('../lib/Shape.js')
 
 test('Should render a blue triangle', () => {
-    const shape = new Triangle();
-    shape.render("blue");
-    expect(shape.render()).toEqual(`<svg width ="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <polygon points="150, 18 244, 182 56, 182" fill="blue" />
-    </svg>`);
+    const shape = new Triangle("blue");
+    shape.render();
+    expect(shape.render()).toEqual(
+        `<svg width ="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150, 18 244, 182 56, 182" fill="blue" />
+      </svg>`);
 })
