4dc4953f88f91d509a1c85808796028e77743b04
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-06-19 15:34:27 -0300

    did the testing for all shapes

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -8,3 +8,21 @@ test('Should render a blue triangle', () => {
       <polygon points="150, 18 244, 182 56, 182" fill="blue" />
       </svg>`);
 })
+
+test('Should render a red square', () => {
+    const shape = new Square("red");
+    shape.render();
+    expect(shape.render()).toEqual(
+        `<svg width ="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <rect x="10" y="10" width="200" height="200" fill="red"/>
+        </svg>`);
+})
+
+test('Should render a #fcba03 circle', () => {
+    const shape = new Circle("#fcba03");
+    shape.render();
+    expect(shape.render()).toEqual(
+        `<svg width ="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="100" fill="#fcba03"/>
+        </svg>`);
+})
